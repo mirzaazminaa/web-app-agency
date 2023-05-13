955ce54e82d48623204b242bbe2a60bcca05e9aa
mirzaazminaa <118162916+mirzaazminaa@users.noreply.github.com>
2023-05-13 17:11:51 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,18 @@
+<?php 
+	include 'header.php';
+ ?>
+
+
+<div class="container" style="padding-bottom: 300px;">
+	<h2 style=" width: 100%; border-bottom: 4px solid #ff8680"><b>Tentang Kami</b></h2>
+	<p class="text-justify">Atlet gymnastics yang kami wakili telah mencatat sejarah dalam olahraga ini dan meraih medali emas, perak, dan perunggu dalam kompetisi tingkat dunia. Kami sangat bangga dengan prestasi mereka dan terus berkomitmen untuk membantu mereka meraih kesuksesan di masa depan. </p>
+<p>
+Dalam peran kami sebagai agensi, kami tidak hanya memberikan dukungan finansial dan pemasaran untuk para atlet kami, tetapi juga memberikan mereka sumber daya yang mereka butuhkan untuk mencapai potensi penuh mereka. Kami bekerja sama dengan para pelatih dan staf pendukung untuk memastikan bahwa atlet kami memiliki pelatihan dan peralatan terbaik untuk mempersiapkan diri mereka dengan baik untuk kompetisi. </p>
+</div>
+
+
+
+
+ <?php 
+	include 'footer.php';
+ ?>
